916810d6229f8691e181dfd7c175b9bfee635b47
Stenia Felix <steniafelix@gmail.com>
2019-08-19 23:25:28 -0300

    adding className

--- src/components/PostHeader/PostHeader.js
+++ src/components/PostHeader/PostHeader.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const PostHeader = ({ users }) => (
-  <div>
+  <div className="postheader">
     {users.map(user => (
-      <div>
+      <div className="itens">
         <img src={user.img} />
         <h1>{user.name}</h1>
-        <p>{user.mensage}</p>
+        <small>{user.time}</small>
+        <div className="mensage">
+          <p>{user.mensage}</p>
+        </div>
       </div>
     ))}
   </div>
